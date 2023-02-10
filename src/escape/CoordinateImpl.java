a6239cdd92f32d5e81cc4c16704b0c2f46adb4ed
dnguyen2065 <dnguyen2065@gmail.com>
2023-02-10 02:11:44 -0500

    coordimpl done

--- src/escape/CoordinateImpl.java
+++ src/escape/CoordinateImpl.java
@@ -1,4 +1,41 @@
 package escape;
 
-public class CoordinateImpl {
+
+import escape.builder.LocationInitializer;
+import escape.required.Coordinate;
+
+public class CoordinateImpl implements Coordinate {
+
+    private int x;
+    private int y;
+
+
+    public CoordinateImpl(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public CoordinateImpl(LocationInitializer li) {
+        this.x = li.x;
+        this.y = li.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof CoordinateImpl coordinate)) {
+            return false;
+        }
+        return x == coordinate.x && y == coordinate.y;
+    }
 }
+
+
+
+
+
+
+
+
+
